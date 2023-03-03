0a2c1ce59979c03be2227339371ab582ef604945
Enric Trenric_trillo <enric_trillo@outlook.com>
2023-03-03 13:40:32 +0000

    c: modded accordion

--- components/accordion.tsx
+++ components/accordion.tsx
@@ -18,7 +18,7 @@ const AccordionDemo = () => (
     <AccordionItem value="item-2">
       <AccordionTrigger>Is it unstyled?</AccordionTrigger>
       <AccordionContent>
-        Yes. It's unstyled by default, giving you freedom over the look and feel.
+        Yes. It&apos;s unstyled by default, giving you freedom over the look and feel.
       </AccordionContent>
     </AccordionItem>
 
