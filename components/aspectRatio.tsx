379854774653108b4d71ea97eb50a0b346afb975
Enric Trenric_trillo <enric_trillo@outlook.com>
2023-03-03 13:40:03 +0000

    c: modded aspectratio

--- components/aspectRatio.tsx
+++ components/aspectRatio.tsx
@@ -5,7 +5,7 @@ const AspectRatioDemo = () => (
   <div className="shadow-blackA7 w-[300px] overflow-hidden rounded-md shadow-[0_2px_10px]">
     <AspectRatio.Root ratio={16 / 9}>
       <img
-        className="h-full w-full object-cover"
+        className="object-cover w-full h-full"
         src="https://images.unsplash.com/photo-1535025183041-0991a977e25b?w=300&dpr=2&q=80"
         alt="Landscape photograph by Tobias Tullius"
       />
